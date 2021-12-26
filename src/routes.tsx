77c6a74751b26169294f2d47e342cebdde6d9a4d
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2021-12-26 17:40:28 -0300

    feat: create home page

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,13 +2,20 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import Home from './pages/Home';
+
 const AppStack = createNativeStackNavigator();
 
 const Routes = () => {
   return (
     <NavigationContainer>
-      <AppStack.Navigator>
-        {/* <AppStack.Screen name="" component={} /> */}
+      <AppStack.Navigator screenOptions={{
+        headerShown: false,
+        headerStyle: {
+          backgroundColor: '#f0f0f5',
+        },
+      }}>
+        <AppStack.Screen name="Home" component={Home} />
       </AppStack.Navigator>
     </NavigationContainer>
   );
